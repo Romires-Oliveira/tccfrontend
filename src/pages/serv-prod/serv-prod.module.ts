68b6f52b9005c0e7693928f5a51d8455b7554f05
Romirés Oliveira <romires2015@gmail.com>
2019-10-21 22:01:35 -0300

    Tela login e tela de exibição de serviço e produto

---
+++ src/pages/serv-prod/serv-prod.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ServProdPage } from './serv-prod';
+
+@NgModule({
+  declarations: [
+    ServProdPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ServProdPage),
+  ],
+})
+export class ServProdPageModule {}
